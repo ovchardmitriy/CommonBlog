f9547d2d1f1c45588658b0d0c4f786154c51c9b6
ovchar-dmitriy <ovchar.dmitriy@gmail.com>
2015-11-14 17:27:01 +0200

    add async setting user's authentication flags to MainCtrl

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('commonBlogApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.posts = [];
-
-    $http.get('/api/posts').success(function(posts) {
-      $scope.posts = posts;
-    });
+  .controller('MainCtrl', function ($scope, Auth) {    
+    $scope.isLoggedInResolved = false;
+    $scope.isLoggedIn = false;    
+  
+    /**
+     * Set flags of the user's authentication
+     *
+     * @param  {Boolean}  isLoggedIn
+     */
+    function setIsLoggedIn(isLoggedIn) {
+      $scope.isLoggedInResolved = true;
+      $scope.isLoggedIn = isLoggedIn;    
+    }
+  
+    Auth.isLoggedInAsync(setIsLoggedIn);
   });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -1,51 +1,62 @@
 'use strict';
 
+// Mocked service Auth
+angular.module('mock.auth', []).
+    factory('Auth', function() {
+        var authService = {},
+            cbArgumnet = false;
+
+        authService.isLoggedInAsync = function(cb) {
+            cb(cbArgumnet);
+        };
+  
+        authService.setCbArgumnet = function(value) {
+            cbArgumnet = value;
+        };
+        
+        return authService;
+    });
+
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('commonBlogApp'));
+  
+  // load mocked service module
+  beforeEach(module('mock.auth'));
 
   var MainCtrl,
-      scope,
-      $httpBackend,
-      posts = [
-        {
-          "_id": "1",
-          "content": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
-          "__v": 0,
-          "createdAt": "2015-11-10T22:00:00.000Z",
-          "title": "Post #1"
-        },
-        {
-          "_id": "2",
-          "content": "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
-          "__v": 0,
-          "createdAt": "2015-11-09T22:00:00.000Z",
-          "title": "Post #2"
-        },
-        {
-          "_id": "3",
-          "content": "Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.",
-          "__v": 0,
-          "createdAt": "2015-11-11T22:00:00.000Z",
-          "title": "Post #3"
-        }
-      ];
+      scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/posts')
-      .respond(posts);
-
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+  it('should set authentication flag to true', function () {
+    // Initialize the controller, mock scope and Auth service
+    inject(function (_Auth_, $controller, $rootScope) {      
+      _Auth_.setCbArgumnet(true);
+      
+      scope = $rootScope.$new();
+      MainCtrl = $controller('MainCtrl', {
+        $scope: scope,
+        Auth: _Auth_
+      });
     });
-  }));
-
-  it('should attach a list of posts to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.posts.length).toBe(3);
+           
+    expect(scope.isLoggedIn).toBe(true);
+    expect(scope.isLoggedInResolved).toBe(true);
+  });
+  
+  it('should set authentication flag to false', function () {
+    // Initialize the controller, mock scope and Auth service
+    inject(function (_Auth_, $controller, $rootScope) {      
+      _Auth_.setCbArgumnet(false);
+      
+      scope = $rootScope.$new();
+      MainCtrl = $controller('MainCtrl', {
+        $scope: scope,
+        Auth: _Auth_
+      });
+    });
+           
+    expect(scope.isLoggedIn).toBe(false);
+    expect(scope.isLoggedInResolved).toBe(true);
   });
 });
