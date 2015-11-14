2c0253d300a9d199a04363e0f29317e6cc44ee85
ovchar-dmitriy <ovchar.dmitriy@gmail.com>
2015-11-14 11:55:45 +0200

    add access to posts on server only for authenticated users

--- server/api/post/post.spec.js
+++ server/api/post/post.spec.js
@@ -3,18 +3,44 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var User = require('../user/user.model');
+var auth = require('../../auth/auth.service.js');
+
 
 describe('GET /api/posts', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with status 401 if user isn\'t authenticated', function(done) {
     request(app)
       .get('/api/posts')
-      .expect(200)
-      .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.should.be.instanceof(Array);
+        res.statusCode.should.be.exactly(401);
         done();
-      });
+    });
+  });
+  
+  it('should respond with JSON array if user is authenticated', function(done) {  
+    var user = new User({
+      provider: 'local',
+      name: 'Fake User',
+      email: 'test@test.com',
+      password: 'password'
+    });
+    var token;
+    
+    user.save(function() {
+      token = auth.signToken(user._id, user.role);
+      
+      request(app)
+        .get('/api/posts')
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });    
   });
 });
